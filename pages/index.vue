dc133d4c44edf12a13837b0325824929fa7458c5
Keyoshi083 <gt.chicken1@gmail.com>
2022-07-24 22:20:06 +0900

    非同期通信の実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,53 @@
 <template>
   <section class="container">
-    <p>{{ message }}</p>
-    <hr />
-    <router-link to="/price">Price Page</router-link>
-    <router-link to="/myPage">My Page</router-link>
-    <router-link to="/users/">User Page</router-link>
-    <router-link to="/users/other">Other Users Page</router-link>
+    <ul>
+      <li v-for="comment in comments" :key="comment.id">
+        {{ comment.id }} , {{ comment.name }} , {{ comment.email }}
+      </li>
+    </ul>
   </section>
 </template>
 
 <script lang="ts">
+import { NuxtAxiosInstance } from "@nuxtjs/axios";
+import axios from "axios";
 import Vue from "vue";
 
-export default Vue.extend({
-  name: "IndexPage",
-  data: () => {
-    return {
-      message: "Hello , World !",
-    };
+const url = "https://jsonplaceholder.typicode.com/posts/1/commentss";
+
+export default {
+  // data() {
+  //   return {
+  //     comments: {},
+  //   };
+  // },
+  // mounted() {
+  //   axios.get(url).then((res) => (this.comments = res.data));
+  // },
+  // : {
+  //   async fetchComments() {
+  //     const comments = await axios.get(url);
+  //     return comments;
+  //   },
+  // },
+  /**
+   * Nuxtの非同期処理
+   * @param param0
+   */
+  asyncData({ params, error }) {
+    return axios
+      .get(url)
+      .then((res) => {
+        return { comments: res.data };
+      })
+      .catch((err) => {
+        // console.error(err.response.status);
+        error({ statusCode: err.response.status, message: err.message });
+      });
   },
-});
+};
+// asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+
+// },
+}
 </script>
